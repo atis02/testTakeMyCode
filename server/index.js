5ef85074f5bc6d5269b70ced941b5315f9158e91
atis02 <guwancikramow@gmail.com>
2025-03-05 13:16:20 +0500

    to vercel

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { PORT } = require("./config");
-const sequelize = require("./db");
 const cors = require("cors");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
@@ -14,14 +13,9 @@ app.use(errorHandler);
 
 const start = async () => {
   try {
-    await sequelize.authenticate();
-    await sequelize.sync({ alter: true });
-    console.log(
-      "Connection to the database has been established successfully."
-    );
     app.listen(PORT, () => console.log(`server started on port ${PORT} `));
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error("Unable to connect:", error);
   }
 };
 start();
